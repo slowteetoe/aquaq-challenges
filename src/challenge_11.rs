1fce4cd570a62eb601efb5c86c08cbb9ff469f8e
Steven Lotito <steven.lotito@gmail.com>
2025-01-24 07:52:44 -0800

    11 WIP, don't like approach

---
+++ src/challenge_11.rs
@@ -0,0 +1,64 @@
+use std::fs;
+
+fn parse_input(input: &str) -> Vec<((usize, usize), (usize, usize))> {
+    input
+        .lines()
+        .skip(1)
+        .map(|line| {
+            let n: Vec<_> = line.splitn(4, ",").collect();
+            (
+                (n[0].parse().unwrap(), n[1].parse().unwrap()),
+                (n[2].parse().unwrap(), n[3].parse().unwrap()),
+            )
+        })
+        .collect()
+}
+
+type Grid = [[u8; 25]; 25];
+
+fn overlapping_shapes(grid: &mut Grid) {
+    let mut keep = false;
+    for x in 0..25 {
+        for y in 0..25 {
+            // walk the shape, gathering up all points and keeping or discarding based on if we see >= 2
+        }
+    }
+}
+
+pub fn solve() -> u32 {
+    let input = fs::read_to_string("./inputs/11.txt").expect("input file");
+    let mut grid: Grid = [[0u8; 25]; 25];
+    let data = parse_input(&input);
+    data.iter().for_each(|(a, b)| {
+        for x in a.0..b.0 {
+            for y in a.1..b.1 {
+                grid[x][y] += 1;
+            }
+        }
+    });
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let input = r"lx,ly,ux,uy
+0,0,3,3
+2,2,4,5
+6,3,8,7";
+        let result = parse_input(input);
+        assert_eq!(
+            result,
+            vec![((0, 0), (3, 3)), ((2, 2), (4, 5)), ((6, 3), (8, 7))]
+        )
+    }
+
+    #[test]
+    fn test_solution() {
+        solve();
+    }
+}
